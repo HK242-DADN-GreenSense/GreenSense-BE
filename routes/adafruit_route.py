ff361cfad9089055c1b51a6adfdbee8edf0cff53
KhoiTranAnh <trananhkhoitv@gmail.com>
2025-03-23 10:39:18 +0700

    update: merge

--- routes/adafruit_route.py
+++ routes/adafruit_route.py
@@ -1,13 +1,8 @@
 from flask import Blueprint, request, jsonify
-from ..controllers.adafruit_controller import ctl_feed_testing, ctl_servo
+from ..controllers.adafruit_controller import *
 from flasgger import swag_from
 
 ada_fruit = Blueprint('ada_fruit', __name__)
-
-@ada_fruit.route('/apa_fruit/send')
-def route_ada_fruit_send():
-    return ctl_feed_testing()
-  
 
 @ada_fruit.route('/api/adafruit/pump/on')
 def route_adafruit_pump_on():
