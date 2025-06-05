78af5a88f3d6df3853edf3fc62bb833bcad69136
KhoiTranAnh <trananhkhoitv@gmail.com>
2025-06-05 14:33:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,6 @@ LOG_DIR = os.path.join(WORKING_DIR, 'var', 'log')
 IOT_ENV = os.path.join(WORKING_DIR, "iot_env.json")
 
 """ Config for GreenSense backend package """
-# MONGO_URI               = "mongodb+srv://tnpht7404:aKrKJMMRyXyw345G@cluster0.c5yym.mongodb.net/"
-MONGO_URI                 = "mongodb+srv://greensense_dev:641abbPRVszJJrbT@cluster0.yvtnh.mongodb.net"
-ADAFRUIT_AIO_USERNAME   = "hieuduongk22bk"
-ADAFRUIT_AIO_KEY        = "aio_TESy52RYyQKkj0g1CsaOUOKUTKPU"
-
-
-# ADAFRUIT_AIO_USERNAME = "trananhkhoitv"
-# ADAFRUIT_AIO_KEY      = "aio_CNzr15m68WWUQ8iAb8RarG68ffFL"
+MONGO_URI                 = "MONGO_URI"
+ADAFRUIT_AIO_USERNAME   = "ADAFRUIT_AIO_USERNAME"
+ADAFRUIT_AIO_KEY        = "ADAFRUIT_AIO_KEY"
